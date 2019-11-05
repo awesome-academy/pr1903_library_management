8833f0fb243e5079e6fd7d6753b56aef601d35e6
Bach <hoangbachilt@gmail.com>
2019-11-05 19:40:22 +0700

    feat(sign_in): Sign in with gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root "pages#index"
   devise_for :users
+  resources :users, only: [:show]
+  as :user do
+    get "signin" => "devise/sessions#new"
+    post "signin" => "devise/sessions#create"
+    delete "signout" => "devise/sessions#destroy"
+    get "signup" => "devise/users#new"
+    post "signup" => "devise/users#create"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
