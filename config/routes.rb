8cc08001c9376c7933b30af598b6cde33b14c94a
Bach <hoangbachilt@gmail.com>
2019-11-07 17:48:26 +0700

    feat(category): View list category

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     get "signup" => "devise/users#new"
     post "signup" => "devise/users#create"
   end
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
