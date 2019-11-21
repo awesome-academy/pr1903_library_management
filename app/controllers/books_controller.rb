d53f663e6861126e13cdd57ddf59e90666d003bd
Tuananh9x <tuananh9x.gha@gmail.com>
2019-11-21 20:07:29 +0700

    feat(server):view infor of book and search books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.paginate(page: params[:page])
+    @books = Book.search(params[:name], params[:page])
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,12 @@ class Book < ApplicationRecord
   has_many :request_details
   has_many :follow_books
   has_many :comments
+
+def self.search(name, page)
+    if name
+      where('name LIKE ?', "%#{name}%").paginate(page: page)
+    else
+      paginate(page: page)
+    end
+  end
 end
