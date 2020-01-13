77677cc5bed9e81b97e3cb63e6d3ad27869b0dce
Bach <hoangbachilt@gmail.com>
2020-01-13 20:40:07 +0700

    feat(fix2): fix second time

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
-  root "pages#index"
-  devise_for :users
+  mount Ckeditor::Engine => '/ckeditor'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root "books#index"
+  devise_for :users, controllers:{omniauth_callbacks: "users/omniauth_callbacks"}
   resources :users, only: [:show]
   as :user do
     get "signin" => "devise/sessions#new"
     post "signin" => "devise/sessions#create"
-    delete "signout" => "devise/sessions#destroy"
+    delete "signout" => "sessions/sessions#destroy"
     get "signup" => "devise/users#new"
     post "signup" => "devise/users#create"
   end
   resources :categories
+  resources :books do 
+    resources :comments
+    resources :likes
+    resources :request_details
+    resources :follow_books
+  end
+  resources :request_borrows
+  resources :authors do
+    resources :follow_authors
+  end
+  resources :publishers
+  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
 category = Category.create! title: "Toan"
 
 10.times.each{|x| Category.create(  title: "Toan#{x}")}
+
+author = Author.create! name: "Leptoiston", intro_author: "nha van nguoi nga"
+
+publisher = Publisher.create! name: "NXB Lam Dong", intro_publisher: "nha xuat ban tuoi tre"
+
+book = Book.create!(author_id: 1,
+                    category_id: 1,
+                    publisher_id: 1,
+                    name: "Dau pha thuong khung",
+                    content: "Day la quyen sach noi ve cuoc doi cua Tieu Viem",
+                    quanity: 30)
