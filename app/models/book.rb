a15a286a1584ae51e45fe94ce450dc83bbe4fda3
Bach <hoangbachilt@gmail.com>
2020-01-13 19:25:59 +0700

    feat(borrow): borrow book from date to date

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,21 @@
 class Book < ApplicationRecord
   has_one_attached :picture
   belongs_to :author
-  has_many :request_details
-  has_many :follow_books
-  has_many :comments
+  belongs_to :category
+  belongs_to :publisher
+  has_many :request_details, dependent: :destroy
+  has_many :follow_books, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  validate :set_quanity
+
+
+  scope :check_quanity, -> { where("quanity >= 0") }
+
+  private
+  def set_quanity
+    if self.quanity <= 0
+      self.status = 0
+    end
+  end
 end
---
+++ app/models/cart.rb
@@ -0,0 +1,4 @@
+class Cart < ApplicationRecord
+  has_many :request_details, dependent: :destroy
+  has_many :books, through: :request_details
+end
